d3d8a2184c3b539d061667e722a6275787d11612
Jamon Holmgren <jamon@infinite.red>
2016-03-05 09:18:24 -0800

    Fleshing out the Net module

--- web/static/utilities/net.js
+++ web/static/utilities/net.js
@@ -1,15 +1,44 @@
 import 'whatwg-fetch' // Polyfill for fetch
 
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+/*
+  Usage:
+
+    Net.post('/thesis/something', {my: "body"})
+      .then((response) => { console.log(response) })
+      .catch((error) => { console.log(error) })
+*/
 const Net = {
-  put: (path, body) => {
+  get: (path, body) => request(path, body, 'GET'),
+  post: (path, body) => request(path, body, 'POST'),
+  put: (path, body) => request(path, body, 'PUT'),
+  patch: (path, body) => request(path, body, 'PATCH'),
+  delete: (path, body) => request(path, body, 'DELETE'),
+  request: (path, body, method) => {
     return fetch(path, {
-      method: 'put',
-      headers: {
+      method: method,
+      credentials: 'same-origin',
+      headers: new Headers({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-      },
+      }),
       body: JSON.stringify(body)
     })
+    .then(checkStatus)
+    .then(parseJSON)
   }
 }
 
