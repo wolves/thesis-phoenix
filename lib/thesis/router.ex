c820ec208a189592dc99a9163f6bff198a20585c
Jamon Holmgren <jamon@infinite.red>
2016-03-08 17:41:05 -0800

    Add pipeline to router

--- lib/thesis/router.ex
+++ lib/thesis/router.ex
@@ -4,16 +4,22 @@ defmodule Thesis.Router do
     quote do
       use Phoenix.Router
 
-      pipeline :thesis_assets do
+      pipeline :thesis_pipeline do
         plug Plug.Static, at: "/thesis", from: :thesis, gzip: true,
           cache_control_for_etags: "public, max-age=86400",
           headers: [{"access-control-allow-origin", "*"}]
+
+        plug :fetch_session
+        plug :fetch_flash
+        plug :put_secure_browser_headers
+        plug :allow_all_origins
       end
 
       scope "/thesis", Thesis do
-        pipe_through :thesis_assets
+        pipe_through :thesis_pipeline
 
         get "/thesis-editor.js", ApiController, :assets
+
         put "/update", ApiController, :update
       end
     end
