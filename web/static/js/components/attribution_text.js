3d4981484face150b8bb666d5d5b179946ff22e4
Yulian Glukhenko <yulian@clearsightstudio.com>
2016-04-25 22:26:51 -0700

    shortened attribution

--- web/static/js/components/attribution_text.js
+++ web/static/js/components/attribution_text.js
@@ -4,7 +4,8 @@ class AttributionText extends React.Component {
   render () {
     return (
     <div className="attribution">
-      <a href="//github.com/infinitered/thesis-phoenix" target="_blank">Thesis</a> is maintained by <a href="//infinite.red" target="_blank">Infinite Red</a>
+      <a href="//github.com/infinitered/thesis-phoenix" target="_blank" >Thesis</a>
+      is maintained by <a href="//infinite.red" target="_blank">Infinite Red</a>
     </div>
     )
   }
