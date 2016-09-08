6eff3914d758ecd102d405a797409a3880b4ebf3
Jamon Holmgren <jamon@infinite.red>
2016-09-07 21:29:25 -0700

    WIP implementing elm

---
+++ web/elm/App.elm
@@ -0,0 +1,11 @@
+import Html exposing (Html, div, text)
+import Html.App as App
+--import Html.Events exposing (onClick)
+
+main : Program Never
+main =
+  App.beginnerProgram {
+    model = 0,
+  }
+
+
