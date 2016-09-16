34d91cfaf3cfa80b4060b850dec36b8b98274bd7
Jamon Holmgren <jamon@infinite.red>
2016-09-15 18:35:41 -0700

    Uses is_integer/1 to avoid nil > 0 issue

--- apps/example/test/ecto_store_test.exs
+++ apps/example/test/ecto_store_test.exs
@@ -18,25 +18,24 @@ defmodule EctoStoreTest do
     :ok = store.update(%{"slug" => "/asdf"}, [pc4, pc5])
 
     page1 = store.page("/test")
-    assert page1.id > 0
+    assert is_integer(page1.id)
     assert page1.slug == "/test"
 
     page2 = store.page("/asdf")
-    assert page2.id > page1.id
+    assert is_integer(page2.id)
     assert page2.slug == "/asdf"
 
     contents = store.page_contents("/test")
 
     assert Enum.at(contents, 0).content == "<h1>Yay</h1>"
-    assert Enum.at(contents, 0).page_id > 0
+    assert is_integer(Enum.at(contents, 0).page_id)
     assert Enum.at(contents, 1).content == "Yo"
-    assert Enum.at(contents, 1).page_id > 0
+    assert is_integer(Enum.at(contents, 1).page_id)
     assert Enum.at(contents, 2).page_id == nil
     assert Enum.at(contents, 2).content == "Wat"
     assert Enum.at(contents, 3).page_id == nil
     assert Enum.at(contents, 3).content == "<p>Other global</p>"
     assert Enum.count(contents) == 4
-
   end
 
 end
