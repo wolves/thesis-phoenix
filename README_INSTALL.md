c3587e1017e2060d36f9283b75ff9d851e5c2ead
Jamon Holmgren <jamon@infinite.red>
2016-10-27 18:36:28 -0700

    Version 0.0.25

--- README_INSTALL.md
+++ README_INSTALL.md
@@ -6,7 +6,7 @@ For automatic setup, see `README.md`.
 
 ```elixir
 def deps do
-  [{:thesis, "~> 0.0.24"}]
+  [{:thesis, "~> 0.0.25"}]
 end
 
 def application do
--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule Thesis.Mixfile do
   use Mix.Project
-  @version "0.0.24" # REMEMBER TO UPDATE package.json and both READMEs!
+  @version "0.0.25" # REMEMBER TO UPDATE package.json and both READMEs!
 
   def project do
     [
