86eb53cfd20a2247943316ce43fd3e31475238a4
Jamon Holmgren <jamon@infinite.red>
2017-09-06 22:27:57 -0700

    Fix apps/example warnings

--- apps/example/test/controllers/api_controller_test.exs
+++ apps/example/test/controllers/api_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule Example.ApiControllerTest do
   use Example.ConnCase
 
-  alias Thesis.{Page, PageContent, Backup}
+  alias Thesis.{Page, Backup}
 
   test "POST /thesis/files/import", %{conn: conn} do
     conn = post conn, "/thesis/files/import", %{"image_url" => "https://cdn-images-1.medium.com/max/128/1*rvw4nIKelSPxYYItLDPy_g@2x.png"}
--- apps/example/test/ecto_store_test.exs
+++ apps/example/test/ecto_store_test.exs
@@ -13,28 +13,28 @@ defmodule EctoStoreTest do
   defp last_updated(model, limit), do: Repo.all(from x in model, order_by: [desc: x.updated_at], limit: ^limit)
 
   test "Save content to a page not edited before" do
-    slug = random_slug
+    slug = random_slug()
     last_pc_id = last_updated(PageContent) && last_updated(PageContent) || 0
 
-    {:ok, _} = @store.update(valid_static_page(slug), [valid_text_page_content])
+    {:ok, _} = @store.update(valid_static_page(slug), [valid_text_page_content()])
 
     assert last_updated(Page).slug == "/" <> slug
     assert last_updated(PageContent).id > last_pc_id
   end
 
   test "Save global content to a page not edited before" do
-    slug = random_slug
+    slug = random_slug()
 
-    {:ok, _} = @store.update(valid_static_page(slug), [valid_global_content])
+    {:ok, _} = @store.update(valid_static_page(slug), [valid_global_content()])
 
     assert last_updated(Page).slug == "/" <> slug
     assert last_updated(PageContent).page_id == nil
   end
 
   test "Save more than one areas at the same time" do
-    {:ok, _} = @store.update(valid_static_page, [valid_text_page_content, valid_html_page_content, valid_global_content])
+    {:ok, _} = @store.update(valid_static_page(), [valid_text_page_content(), valid_html_page_content(), valid_global_content()])
 
-    assert last_updated(Page).slug == valid_static_page["slug"]
+    assert last_updated(Page).slug == valid_static_page()["slug"]
     last_3 = last_updated(PageContent, 3)
     assert Enum.at(last_3, 0).content_type == "raw_html"
     assert Enum.at(last_3, 1).content_type == "html"
@@ -42,9 +42,9 @@ defmodule EctoStoreTest do
   end
 
   test "Save global area on one page; retrieve on a different page that's not yet in database" do
-    {:ok, _} = @store.update(valid_static_page, [valid_global_content])
+    {:ok, _} = @store.update(valid_static_page(), [valid_global_content()])
 
-    records = @store.page_contents("/" <> random_slug)
+    records = @store.page_contents("/" <> random_slug())
 
     assert records == last_updated(PageContent, 1)
     assert Enum.at(records, 0).content_type == "raw_html"
@@ -52,8 +52,8 @@ defmodule EctoStoreTest do
 
   test "Retrieves page content as well as well as global content saved on a different page" do
     slug = random_slug()
-    {:ok, _} = @store.update(valid_static_page(random_slug()), [valid_global_content])
-    {:ok, _} = @store.update(valid_static_page(slug), [valid_html_page_content])
+    {:ok, _} = @store.update(valid_static_page(random_slug()), [valid_global_content()])
+    {:ok, _} = @store.update(valid_static_page(slug), [valid_html_page_content()])
 
     records = @store.page_contents("/" <> slug)
 
@@ -63,8 +63,8 @@ defmodule EctoStoreTest do
   end
 
   test "First adds page, then deletes page found by slug" do
-    slug = random_slug
-    {:ok, _} = @store.update(valid_static_page(slug), [valid_html_page_content])
+    slug = random_slug()
+    {:ok, _} = @store.update(valid_static_page(slug), [valid_html_page_content()])
 
     assert @store.page("/" <> slug).slug == "/" <> slug
     assert last_updated(Page).slug == "/" <> slug
