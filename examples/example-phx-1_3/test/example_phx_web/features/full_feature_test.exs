92c28fe4784ab865f1bd6e8124387ec6dc7b1c06
Jamon Holmgren <jamon@infinite.red>
2017-09-15 23:03:09 -0700

    End to end tests

--- examples/example-phx-1_3/test/example_phx_web/features/full_feature_test.exs
+++ examples/example-phx-1_3/test/example_phx_web/features/full_feature_test.exs
@@ -6,13 +6,15 @@ defmodule ExamplePhxWeb.FullFeatureTest do
   # Start a Hound session
   hound_session()
 
-
   test "GET /" do
+    # Basic home page, not logged in
     navigate_to "/"
     assert page_source() =~ "Welcome to Thesis"
+    thesis = find_element(:id, "thesis-editor")
+    refute element_displayed?(thesis)
 
+    # Log in
     click({:link_text, "Log in"})
-    
     logout = find_element(:link_text, "Log out")
     assert element_displayed?(logout)
 
@@ -22,61 +24,76 @@ defmodule ExamplePhxWeb.FullFeatureTest do
     save = find_element(:css, ".thesis-button.save")
     refute element_displayed?(save)
 
+    # Open the editor
     click({:css, ".thesis-button.edit"})
-
     wait_for(save, :show)
 
+    # Edit the jumbotron
     jumbotron = find_element(:id, "thesis-content-jumbotron-welcome")
     fill_field(jumbotron, "New content here!")
 
+    # Save the page
     click(save)
-
     wait_for(save, :hide)
 
+    # Refresh and check that the content is still there
     refresh_page()
-
     jumbotron = find_element(:id, "thesis-content-jumbotron-welcome")
     assert inner_html(jumbotron) =~ "<p>New content here!</p>"
 
+    # Hover over edit, click Add
     move_to({:css, ".thesis-button.edit"}, 5, 5)
-
     add = find_element(:css, ".thesis-button.add")
     wait_for(add, :show)
     click(add)
-
+    
+    # Wait for the tray to show up
     path = find_element(:css, "input.page-path")
     title = find_element(:css, "input.page-title")
-    description = find_element(:css, "textarea.page-description")
-    
+    description = find_element(:css, "textarea.page-description")    
+    wait_for(path, :show)
+
+    # Test the cancel button
+    click({:class, "thesis-tray-cancel"})
+    wait_for(path, :hide)
+
+    # Try adding a new page
+    move_to({:css, ".thesis-button.edit"}, 5, 5)
+    wait_for(add, :show)
+    click(add)
     wait_for(path, :show)
     
+    # Fill out the new page form
     rand = random_string(32)
-
     fill_field(path, "/about")
     fill_field(title, "About Us")
     fill_field(description, "Description: #{rand}")
 
+    # Save the new page, let it redirect
     click({:class, "thesis-tray-save"})
-
     :timer.sleep(100)
 
+    # Don't want confirms popping up when we delete
     remove_alerts()
 
+    # Check the newly created page
     jumbotron = find_element(:id, "thesis-content-jumbotron-welcome")
     assert inner_html(jumbotron) =~ "<h2>Welcome to Thesis!</h2>"
     assert page_title() == "About Us"
     assert page_source() =~ "<meta name=\"description\" content=\"Description: #{rand}\" />"
 
+    # Click the delete button
     move_to({:css, ".thesis-button.edit"}, 5, 5)
-
     delete = find_element(:css, ".thesis-button.delete")
     wait_for(delete, :show)
     click(delete)
 
+    # Refresh the page
+    :timer.sleep(100)
+    refresh_page()
     :timer.sleep(100)
 
-    refresh_page()
-
+    # Should get a 404
     assert page_source() =~ "Page not found"
   end
 
@@ -101,6 +118,6 @@ defmodule ExamplePhxWeb.FullFeatureTest do
   end
 
   defp remove_alerts() do
-    execute_script("window.alert=function (a) {};window.prompt=function (a) {};window.confirm=function (a) {};")
+    execute_script("window.alert=function(a){return true};window.prompt=function(a){return true};window.confirm=function(a){return true};")
   end
 end
